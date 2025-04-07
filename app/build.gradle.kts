a826030582115683fa36e3f5e0b66118aca289d5
Pavel Tretyakov <tretyakovpavel54@mail.ru>
2025-04-07 16:20:23 +0200

    new file Makefile,  modified App.java

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    //annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
+    //compileOnly("org.projectlombok:lombok:1.18.24")
 }
 
 application {
